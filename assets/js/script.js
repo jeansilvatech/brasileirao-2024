5819edb3c52a7f4c8f9c81541da5ba63a55ee4cc
jeansilvatech <jeanpsilva10@gmail.com>
2024-02-01 17:50:25 -0300

    Teste slider 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,29 +4,33 @@ const data = await res.json();
 data.forEach(team => {
     headerTeams.innerHTML += `
         <div class="keen-slider__slide team">
-            <img src="./assets/img/${team.url}" alt="">
+            <img src="./assets/img/${team.url}" alt="escudo do ${team.name}">
         </div>
     `
 });
 var slider = new KeenSlider("#my-keen-slider", {
-    
     breakpoints: {
       "(min-width: 320px)": {
-        slides: { perView: 3},
+        slides: { 
+            perView: 3,
+            spacing:1,
+            origin:3},
       },
       "(min-width: 600px)": {
         slides: { perView: 5},
       },
       "(min-width: 768px)": {
-        slides: { perView: 10},
+        slides: { perView: 10,
+            spacing:1,
+            origin:1},
       },
-      "(min-width: 1000px)": {
-        slides: { perView: 20},
+      "(min-width: 1100px)": {
+        slides: { perView: 20
+        },
       },
     },
     slides: {
-        perView: 1,
-        
+        perView: 1
     },
   })
 
